9007593e3169aace8e6111d44fffb394f6a2d111
wangy <yiwayhb@qq.com>
2017-07-01 22:22:23 +0800

    1

--- CloudContractWebLib/Controllers/ContractController.cs
+++ CloudContractWebLib/Controllers/ContractController.cs
@@ -7,11 +7,6 @@ using ClownFish.Web;
 
 namespace CloudContractWebLib.Controllers
 {
-    /// <summary>
-    /// 合同控制器
-    /// </summary>
-    public class ContractController : BaseController
-    {
 	/// <summary>
 	/// 合同控制器
 	/// </summary>
@@ -26,42 +21,39 @@ namespace CloudContractWebLib.Controllers
 				TemplateGuid = templateGuid,
 			});
 		}
-	}
-        /// <summary>
-        /// 合同列表
-        /// </summary>
-        [PageUrl(Url = "/contract/get-contracts.aspx")]
-        [Action(OutFormat = SerializeFormat.Json, Verb = "POST")]
-        public List<Contract> GetList()
-        {
-            using (var scope = ConnectionScope.GetExistOrCreate())
-            {
-                return CPQuery.Create(@"
+		/// <summary>
+		/// 合同列表
+		/// </summary>
+		[PageUrl(Url = "/contract/get-contracts.aspx")]
+		[Action(OutFormat = SerializeFormat.Json, Verb = "POST")]
+		public List<Contract> GetList()
+		{
+			using( var scope = ConnectionScope.GetExistOrCreate() ) {
+				return CPQuery.Create(@"
 SELECT  ContractGUID ,
         ContractName ,
         ContractTemplateGUID
 FROM    [dbo].[Geek_Contract]
 WHERE   ApproveStatus IS NULL
         OR ApproveStatus = '不通过'").ToList<Contract>();
-            }
-        }
+			}
+		}
 
-        /// <summary>
-        /// 合同保存
-        /// </summary>
-        [PageUrl(Url = "/contract-save.aspx")]
-        [Action(Verb = "POST")]
-        public void Save(Contract contract)
-        {
-            // 入参校验
-            if (contract == null)
-                throw new ArgumentNullException(nameof(contract));
+		/// <summary>
+		/// 合同保存
+		/// </summary>
+		[PageUrl(Url = "/contract-save.aspx")]
+		[Action(Verb = "POST")]
+		public void Save(Contract contract)
+		{
+			// 入参校验
+			if( contract == null )
+				throw new ArgumentNullException(nameof(contract));
 
-            string sql;
+			string sql;
 
-            if (contract.ContractGUID == Guid.Empty)
-            {
-                sql = @"
+			if( contract.ContractGUID == Guid.Empty ) {
+				sql = @"
 INSERT  INTO [dbo].[Geek_Contract]
         ( [ContractGUID] ,
           [CreatedTime] ,
@@ -91,10 +83,9 @@ VALUES  ( NEWID() ,
           @ContractTemplateGUID ,
           @ContractAmount
         )";
-            }
-            else
-            {
-                sql = @"
+			}
+			else {
+				sql = @"
 UPDATE  [dbo].[Geek_Contract]
 SET     [ModifiedTime] = GETDATE() ,
         [ModifiedGUID] = '4230BC6E-69E6-46A9-A39E-B929A06A84E8' ,
@@ -105,12 +96,11 @@ SET     [ModifiedTime] = GETDATE() ,
         [ContractName] = @ContractName ,
         [ContractAmount] = @ContractAmount
 WHERE   ContractGUID = @ContractGUID";
-            }
+			}
 
-            using (var scope = ConnectionScope.GetExistOrCreate())
-            {
-                CPQuery.Create(sql, contract).ExecuteNonQuery();
-            }
-        }
-    }
+			using( var scope = ConnectionScope.GetExistOrCreate() ) {
+				CPQuery.Create(sql, contract).ExecuteNonQuery();
+			}
+		}
+	}
 }
