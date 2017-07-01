a942db61d0bd2cc21e3150d91d60d1530d91259b
chenb02 <beenchen@vip.qq.com>
2017-07-01 16:41:21 +0800

    模板保存

--- CloudContractWeb/static/Template/addnew.js
+++ CloudContractWeb/static/Template/addnew.js
@@ -1,11 +1,42 @@
-﻿function saveTemplate() {
-    var name = $("#name").getValue();
+﻿(function() {
+    "use strict";
 
-    if (!name) {
-        alert("模板名称不能为空");
+    // 每次加载新页面时均必须运行初始化函数
+    Office.initialize = function(reason) {
+        $(document).ready(function() {
+            app.initialize();
+            
+            $("#btn-save").click(saveTemplate);
+        });
+    };
+
+    function saveTemplate() {
+        var name = $("#name").val();
+
+        if (name === "") {
+            app.showNotification("模板名称不能为空");
+            return;
+        }
+
+        Word.run(function (context) {
+            var body = context.document.body;
+            var bodyXml = body.getOoxml();
+
+            return context.sync().then(function() {
+                $.post("/template-save.aspx",
+                    {
+                        Content: bodyXml.value,
+                        Name: name
+                    })
+                    .done(function() {
+                        app.showNotification("保存成功！");
+                    })
+                    .error(function(error) {
+                        app.showNotification("Error:", JSON.stringify(error));
+                    });
+            });
+        }).catch(function (error) {
+            app.showNotification("Error:", JSON.stringify(error));
+        });
     }
-
-    $.post("/template-save.aspx", { Name: name, Content: {} }, function () {
-        alert("保存模板成功");
-    });
-}
+})();
