75a3e272de0c020c53ded8e14c5fae1e452e8b57
chenb02 <beenchen@vip.qq.com>
2017-07-01 21:52:44 +0800

    合同列表

--- CloudContractWebLib/Controllers/ContractController.cs
+++ CloudContractWebLib/Controllers/ContractController.cs
@@ -1,4 +1,9 @@
-﻿using ClownFish.Web;
+﻿using System;
+using System.Collections.Generic;
+using CloudContractWebLib.Models;
+using ClownFish.Base.Http;
+using ClownFish.Data;
+using ClownFish.Web;
 
 namespace CloudContractWebLib.Controllers
 {
@@ -7,10 +12,90 @@ namespace CloudContractWebLib.Controllers
     /// </summary>
     public class ContractController : BaseController
     {
-        [PageUrl(Url = "/contract-index.aspx")]
-        public IActionResult Index()
+        /// <summary>
+        /// 合同列表
+        /// </summary>
+        [PageUrl(Url = "/contract/get-contracts.aspx")]
+        [Action(OutFormat = SerializeFormat.Json, Verb = "POST")]
+        public List<Contract> GetList()
         {
-            return new PageResult("~/views/Template/index.cshtml");
+            using (var scope = ConnectionScope.GetExistOrCreate())
+            {
+                return CPQuery.Create(@"
+SELECT  ContractGUID ,
+        ContractName ,
+        ContractTemplateGUID
+FROM    [dbo].[Geek_Contract]
+WHERE   ApproveStatus IS NULL
+        OR ApproveStatus = '不通过'").ToList<Contract>();
+            }
+        }
+
+        /// <summary>
+        /// 合同保存
+        /// </summary>
+        [PageUrl(Url = "/contract-save.aspx")]
+        [Action(Verb = "POST")]
+        public void Save(Contract contract)
+        {
+            // 入参校验
+            if (contract == null)
+                throw new ArgumentNullException(nameof(contract));
+
+            string sql;
+
+            if (contract.ContractGUID == Guid.Empty)
+            {
+                sql = @"
+INSERT  INTO [dbo].[Geek_Contract]
+        ( [ContractGUID] ,
+          [CreatedTime] ,
+          [CreatedGUID] ,
+          [CreatedName] ,
+          [ModifiedTime] ,
+          [ModifiedGUID] ,
+          [ModifiedName] ,
+          [FirstPattern] ,
+          [SecondPattern] ,
+          [ThirtyPattern] ,
+          [ContractName] ,
+          [ContractTemplateGUID] ,
+          [ContractAmount]
+        )
+VALUES  ( NEWID() ,
+          GETDATE() ,
+          '4230BC6E-69E6-46A9-A39E-B929A06A84E8' ,
+          '系统管理员' ,
+          GETDATE() ,
+          '4230BC6E-69E6-46A9-A39E-B929A06A84E8' ,
+          '系统管理员' ,
+          @FirstPattern ,
+          @SecondPattern ,
+          @ThirtyPattern ,
+          @ContractName ,
+          @ContractTemplateGUID ,
+          @ContractAmount
+        )";
+            }
+            else
+            {
+                sql = @"
+UPDATE  [dbo].[Geek_Contract]
+SET     [ModifiedTime] = GETDATE() ,
+        [ModifiedGUID] = '4230BC6E-69E6-46A9-A39E-B929A06A84E8' ,
+        [ModifiedName] = '系统管理员' ,
+        [FirstPattern] = @FirstPattern ,
+        [SecondPattern] = @SecondPattern ,
+        [ThirtyPattern] = @ThirtyPattern ,
+        [ContractName] = @ContractName ,
+        [ContractAmount] = @ContractAmount
+WHERE   ContractGUID = @ContractGUID";
+            }
+
+            using (var scope = ConnectionScope.GetExistOrCreate())
+            {
+                CPQuery.Create(sql, contract).ExecuteNonQuery();
+            }
         }
     }
 }
